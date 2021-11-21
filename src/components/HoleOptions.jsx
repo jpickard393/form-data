762434aae3dd4e592722c09f1d33aa3a8eff4e57
jpickard393 <jpickard@and.digital>
2021-11-21 12:34:56 +0000

    uses state for button no form data

--- src/components/HoleOptions.jsx
+++ src/components/HoleOptions.jsx
@@ -1,5 +1,38 @@
-import React from "react";
-const HoleOptions = () => {
-  return <div></div>;
+import React, { useState } from "react";
+import { ButtonGroup, ToggleButton } from "react-bootstrap";
+const HoleOptions = ({ selectionData, handleOnChange }) => {
+  const [radioValue, setRadioValue] = useState("");
+  const radios = [
+    { name: "Round", value: "1" },
+    { name: "Square", value: "2" },
+  ];
+
+  const setChecked = (e) => {
+    console.log(e.value);
+    console.log(selectionData);
+    setRadioValue(e.currentTarget.value);
+    return radioValue === e.value;
+  };
+
+  return (
+    <div>
+      <ButtonGroup>
+        {radios.map((radio, idx) => (
+          <ToggleButton
+            key={idx}
+            id={`radio-${idx}`}
+            type="radio"
+            variant={idx % 2 ? "outline-success" : "outline-danger"}
+            name="radio"
+            value={radio.value}
+            checked={radioValue === radio.value}
+            onChange={(e) => setRadioValue(e.currentTarget.value)}
+          >
+            {radio.name}
+          </ToggleButton>
+        ))}
+      </ButtonGroup>
+    </div>
+  );
 };
 export default HoleOptions;
--- src/components/MainForm.jsx
+++ src/components/MainForm.jsx
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import HoleOptions from "./HoleOptions";
-import PegOptions from "./PegOptions";
-import Message from "./Message";
+// import PegOptions from "./PegOptions";
+// import Message from "./Message";
+
+const setInitialSelectionValues = () => ({
+  peg: "",
+  hole: "",
+  useSledgeHammer: false,
+});
+
 const MainForm = () => {
-  return <div></div>;
+  const [selectionData, setSelectionData] = useState(
+    setInitialSelectionValues()
+  );
+
+  const handleOnChange = (name) => (data) => {
+    const resolvers = {
+      string: (data) => data,
+      object: (data) => ("value" in data ? data.value : data),
+    };
+    const inputType = typeof data;
+    setSelectionData((state) => ({
+      ...state,
+      [name]: resolvers[inputType]?.(data),
+    }));
+  };
+
+  const formData = { selectionData, handleOnChange };
+
+  console.log(selectionData);
+
+  return <HoleOptions {...formData}></HoleOptions>;
 };
 export default MainForm;
